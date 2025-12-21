65e7c3864f56df12e30f76a8c9782ae0d4eafcba
Bellum17 <benjamin_claude_@hotmail.com>
2025-12-21 22:07:19 +0100

    Ajout page d'accueil et restructuration du site

--- script.js
+++ script.js
@@ -1,56 +1,79 @@
 // --- 1. Initialisation de la carte ---
-// On cible la div avec l'id 'maCarte'
-// Coordonnées approximatives du centre de l'Égypte [Latitude, Longitude] et niveau de zoom (6)
-var map = L.map('maCarte').setView([26.8206, 30.8025], 6);
+// Carte centrée sur la région 39 (Nord de l'Égypte - Delta du Nil)
+// Définition des limites pour toute l'Égypte
+var southWest = L.latLng(22.0, 25.0);  // Coin sud-ouest (frontière soudanaise)
+var northEast = L.latLng(31.8, 35.0);  // Coin nord-est (Sinaï)
+var bounds = L.latLngBounds(southWest, northEast);
+
+// Initialisation de la carte avec restrictions
+var map = L.map('maCarte', {
+    center: [30.5, 31.2],
+    zoom: 9,
+    minZoom: 6,
+    maxZoom: 14,
+    maxBounds: bounds,
+    maxBoundsViscosity: 1.0  // Empêche de sortir des limites
+}).setView([30.5, 31.2], 9);
 
 
 // --- 2. Ajout du fond de carte (Tiles) ---
 // Nous utilisons OpenStreetMap (gratuit)
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    maxZoom: 19,
+    maxZoom: 14,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
 
 
-// --- 3. Ajout d'éléments variés ---
+// --- 3. Icônes personnalisées ---
 
-// ELEMENT A : Un marqueur classique (Le Caire)
-var caireMarker = L.marker([30.0444, 31.2357]).addTo(map);
-// On ajoute une popup qui s'ouvre au clic
-caireMarker.bindPopup("<b>Le Caire</b><br>Capitale de l'Égypte.");
+// Icône de gouvernement (GOV)
+var govIcon = L.icon({
+    iconUrl: 'images/icon_gov_rdn.png',
+    iconSize: [60, 50],        // Taille de l'icône (largeur x hauteur) - format rectangulaire
+    iconAnchor: [30, 50],      // Point d'ancrage (centré en bas)
+    popupAnchor: [0, -50]      // Position de la popup
+});
 
+// Groupe de calques pour les symboles gouvernementaux
+var gouvernementLayer = L.layerGroup();
 
-// ELEMENT B : Un cercle (Zone des Pyramides de Gizeh)
-// Le cercle est défini par son centre, son rayon en mètres, et sa couleur
-var gizaCircle = L.circle([29.9792, 31.1342], {
-    color: '#d4af37', // Couleur or pour le contour
-    fillColor: '#d4af37', // Couleur de remplissage
-    fillOpacity: 0.5,     // Transparence
-    radius: 5000          // Rayon de 5km autour des pyramides
-}).addTo(map);
-gizaCircle.bindPopup("<b>Nécropole de Gizeh</b><br>Zone des grandes pyramides.");
+// Placement au Caire
+var caireGov = L.marker([30.0444, 31.2357], {icon: govIcon});
+caireGov.bindPopup("<b>Le Caire</b><br>Capitale du Royaume du Nil<br><span style='color:#87CEEB'>● Gouvernement</span>");
+gouvernementLayer.addLayer(caireGov);
 
-
-// ELEMENT C : Un polygone (Le Delta du Nil - forme approximative)
-// Un polygone est une série de points reliés entre eux
-var deltaPolygon = L.polygon([
-    [31.25, 29.9], // Point Nord-Ouest (Alexandrie approx)
-    [31.3, 32.3],  // Point Nord-Est (Port-Saïd approx)
-    [30.1, 31.2]   // Point Sud (Pointe du delta près du Caire)
-], {
-    color: 'green',
-    fillColor: '#aaddaa',
-    fillOpacity: 0.4
-}).addTo(map);
-deltaPolygon.bindPopup("<b>Delta du Nil</b><br>Zone agricole fertile.");
+// Ajouter le groupe à la carte par défaut
+gouvernementLayer.addTo(map);
 
 
-// ELEMENT D : Un marqueur circulaire (Vallée des Rois / Louxor)
-// Similaire à un cercle, mais sa taille reste fixe quel que soit le zoom
-var luxorMarker = L.circleMarker([25.7402, 32.6014], {
-    color: 'red',
-    fillColor: '#f03',
-    fillOpacity: 0.8,
-    radius: 10 // Taille fixe en pixels
-}).addTo(map);
-luxorMarker.bindPopup("<b>Louxor & Vallée des Rois</b><br>Haut lieu archéologique.");
+// --- 4. Légende de la carte ---
+var legend = L.control({position: 'bottomright'});
+
+legend.onAdd = function (map) {
+    var div = L.DomUtil.create('div', 'legend');
+    div.innerHTML = '<h4>Légende</h4>';
+    div.innerHTML += '<div class="legend-item"><span class="legend-color" style="background-color: #80e0ff;"></span> Symboles Gouvernementaux</div>';
+    return div;
+};
+
+legend.addTo(map);
+
+
+// --- 5. Menu Burger et Filtres ---
+const burgerBtn = document.getElementById('burgerBtn');
+const menuContent = document.getElementById('menuContent');
+const filterGouvernement = document.getElementById('filterGouvernement');
+
+// Toggle du menu burger
+burgerBtn.addEventListener('click', function() {
+    menuContent.classList.toggle('menu-hidden');
+});
+
+// Filtre pour les symboles gouvernementaux
+filterGouvernement.addEventListener('change', function() {
+    if (this.checked) {
+        gouvernementLayer.addTo(map);
+    } else {
+        map.removeLayer(gouvernementLayer);
+    }
+});
